7c6a06c7a8f68349e81c9875c8475c9a164e8a04
Aaron Beckett <aminor65ii@gmail.com>
2016-10-30 09:39:10 -0400

    Add beginning of heapify

---
+++ algorithms/heapify.py
@@ -0,0 +1,20 @@
+
+import math
+
+def heapify(A):
+
+    def left(i):
+        return i*2 + 1
+
+    def right(i):
+        return i*2 + 2
+
+    start = len(A) // 2
+
+def extract_max(heap):
+    pass
+
+def heap_insert(heap):
+
+    def parent(i):
+        return math.ceil(i/2) - 1
